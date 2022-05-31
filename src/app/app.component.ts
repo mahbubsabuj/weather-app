de0dd49f6e526d31e57df114bc76e715b866cd24
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-01 00:46:01 +0600

    feat: :fire: Search Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,33 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { WeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title: string = 'Weather App';
+  city: string | null = '';
   weatherData?: WeatherData;
+
   constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
-    this.weatherService.getWeatherData('dhaka').subscribe({
-      next: (response: WeatherData) => {
-        this.weatherData = response;
-        console.log(response);
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    })
+    
+  }
+  handleSubmit(form: NgForm) {
+    if (form.value.searchQuery) {
+      this.weatherService.getWeatherData(form.value.searchQuery).subscribe({
+        next: (response: WeatherData) => {
+          this.weatherData = response;
+          this.city = '';
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    }
   }
 }
